ebbef9f7f94b577719416b263140c1b0a62e71ef
tomjuran <68550775+tomjuran@users.noreply.github.com>
2020-12-29 12:16:49 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+//Store -> Globalized State
+
+//Action Increment
+//const increment = () => {
+  //return {
+    //type: 'INCREMENT'
+  //}
+//}
+//const decrement = () => {
+  //return {
+    //type: 'DECREMENT'
+  //}
+//}
+
+//Reducer
+//const counter = (state = 0, action) => {
+  //switch(action.type){
+    //case 'INCREMENT':
+      //return state + 1;
+    //case 'DECREMENT':
+      //return state - 1;
+  //}
+
+//};
+
+//let store = createStore(counter);
+
+//Display it in the console
+//store.subscribe(() => console.log(store.getState()));
+
+
+//Dispatch
+//store.dispatch(increment());
+//store.dispatch(decrement());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
